9c547ba68c3c48eee96ea3c72a5f448111c78fec
rathishubham7 <shubhamrathi@icloud.com>
2020-08-19 19:34:56 +0800

    added ui for react select

--- ui/app/pages/torus-restore-password/device-form/device-form.component.js
+++ ui/app/pages/torus-restore-password/device-form/device-form.component.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Button from "../../../components/ui/button";
 import Select from "react-select";
-import Bowser from "bowser"; 
+import { components } from "react-select";
+import Bowser from "bowser";
 import {
   INITIALIZE_END_OF_FLOW_ROUTE,
   DEFAULT_ROUTE
-} from '../../../helpers/constants/routes'
+} from "../../../helpers/constants/routes";
+import ComputerIcon from "@material-ui/icons/Computer";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 export default class DeviceForm extends Component {
   static defaultProps = {
@@ -17,73 +21,188 @@ export default class DeviceForm extends Component {
     inputPassword: "",
     defaultAccountName: "Enter your password here",
     selectedDevice: "New-device",
-    browser: "",
-    devices: []
+    browser: {},
+    devices: [],
+    tabValue: 1
   };
 
   componentDidMount() {
     const { changeHeading, getTotalDeviceShares } = this.props;
-    
-    changeHeading("Verify device"); // for tabs
-    
+
+    changeHeading("Save extension"); // for tabs
+
     this.setDeviceDetails(); // for adding this extension
 
     // Show options with labels
     getTotalDeviceShares().then(devices => {
-      let totalDevices = [{ label: "New device", value: "New-device" }]
+      let totalDevices = [];
       Object.keys(devices).map(index => {
-        devices[index] = devices[index].slice(0,1)
+        devices[index] = devices[index].slice(0, 1);
         return devices[index].map(device => {
-          totalDevices.push({label: this.getBowserLabel(device.userAgent), value: index})
-        })
+          let date = new Date(device.dateAdded)
+          const browser = Bowser.getParser(device.userAgent);
+          totalDevices.push({
+            label: this.getBowserLabel(device.userAgent),
+            value: index,
+            browser: {
+              name: browser.getBrowserName(),
+              os: browser.getOSName(),
+              platform: browser.getPlatformType(),
+              date: date.toDateString(),
+              index: index
+            }
+          });
+        });
         // return {label: getBrowserLabel(devices)}
-      })
+      });
       this.setState({
         devices: totalDevices
-      })
-      console.log(totalDevices)
-    }) // populate list of available devices
+      });
+      console.log(totalDevices);
+    }); // populate list of available devices
   }
 
   getBowserLabel(agent) {
     const browser = Bowser.getParser(agent);
-    return browser.getBrowserName() + " " + browser.getOSName()
+    return browser.getBrowserName() + " " + browser.getOSName();
   }
 
   setDeviceDetails() {
     const browser = Bowser.getParser(navigator.userAgent);
     // console.log(browser)
+    let date = new Date()
+
     this.setState({
-      browser: browser.getBrowserName() + " " + browser.getOSName()
+      browser: {
+        name: browser.getBrowserName(),
+        os: browser.getOSName(),
+        platform: browser.getPlatformType(),
+        date: date.toDateString()
+      }
     });
   }
 
   addDevice = async () => {
-    const { selectedDevice } = this.state
-    const {copyShareUsingIndexAndStoreLocally, generateAndStoreNewDeviceShare, history} = this.props
-    if (selectedDevice === 'New-device') {
+    const { selectedDevice } = this.state;
+    const {
+      copyShareUsingIndexAndStoreLocally,
+      generateAndStoreNewDeviceShare,
+      history
+    } = this.props;
+    if (selectedDevice === "New-device") {
       try {
-        await generateAndStoreNewDeviceShare()
-        history.push(INITIALIZE_END_OF_FLOW_ROUTE)
+        await generateAndStoreNewDeviceShare();
+        history.push(INITIALIZE_END_OF_FLOW_ROUTE);
       } catch (err) {
-        console.error(err)
-        debugger
+        console.error(err);
+        debugger;
+      }
+    } else {
+      try {
+        await copyShareUsingIndexAndStoreLocally(selectedDevice.index);
+        history.push(INITIALIZE_END_OF_FLOW_ROUTE);
+      } catch (err) {
+        console.error(err);
+        debugger;
       }
     }
-    else {
-      try {
-        await copyShareUsingIndexAndStoreLocally(selectedDevice)
-        history.push(INITIALIZE_END_OF_FLOW_ROUTE)
-      } catch (err) {
-        console.error(err)
-        debugger
-      }
-    }
-  }
+  };
 
   continueWithoutAddingDevice = () => {
-    const { history } = this.props
-    history.push(INITIALIZE_END_OF_FLOW_ROUTE)
+    const { history } = this.props;
+    history.push(INITIALIZE_END_OF_FLOW_ROUTE);
+  };
+
+  renderAddNewDevice() {
+    const { browser } = this.state;
+
+    return (
+      <div className="new-account-create-form__device-info">
+        <Grid container>
+          <Grid item xs={2}>
+            <ComputerIcon />
+          </Grid>
+          <Grid item xs={10}>
+            <h4>
+              {browser.os}
+              <span> (Current new device)</span>
+            </h4>
+            <p>{browser.platform + ", " + browser.name}</p>
+            <p>{browser.date}</p>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+
+
+  renderAddOldDevice() {
+    const { devices, selectedDevice } = this.state;
+    // console.log(components)
+    // debugger
+    const { Option, ValueContainer } = components;
+
+    const IconOption = props => {
+      console.log(props)
+      return (
+        <Option {...props}>
+          <div className="new-account-create-form__device-info new-account-create-form__device-option">
+            <Grid container>
+              <Grid item xs={2}>
+                <ComputerIcon />
+              </Grid>
+              <Grid item xs={10}>
+                <h4>
+                  {props.data.browser.os}
+                </h4>
+                <p>{props.data.browser.platform + ", " + props.data.browser.name}</p>
+                <p>{props.data.browser.date}</p>
+              </Grid>
+            </Grid>
+          </div>
+        </Option>
+      );
+    };
+    const ValueOption =  (props) => {
+      // debugger
+      console.log(props.data)
+      return (
+        <ValueContainer {...props}>
+          <div className="new-account-create-form__device-info new-account-create-form__device-option">
+            <Grid container>
+              <Grid item xs={2}>
+                <ComputerIcon />
+              </Grid>
+              <Grid item xs={10}>
+                <h4>
+                  {props.data.os}
+                </h4>
+                <p>{props.data.platform + ", " + props.data.name}</p>
+                <p>{props.data.date}</p>
+              </Grid>
+            </Grid>
+          </div>
+        </ValueContainer>
+      );
+    };
+
+    return (
+      <Select
+        className="new-account-create-form__device-select"
+        name="import-type-select"
+        clearable={false}
+        value={selectedDevice}
+        options={devices}
+        defaultValue={devices[0].browser}
+        // closeMenuOnSelect={false}
+        isSearchable={false}
+        onChange={opt => {
+          console.log(opt);
+          this.setState({ selectedDevice: opt.browser });
+        }}
+        components={{ Option: IconOption, SingleValue: ValueOption }}
+      />
+    );
   }
 
   render() {
@@ -92,55 +211,55 @@ export default class DeviceForm extends Component {
       defaultAccountName,
       selectedDevice,
       browser,
-      devices
+      devices,
+      tabValue
     } = this.state;
-  
+
     return (
       <div className="new-account-create-form">
         <div className="new-account-create-form__input-label">
           Confirm your browser and device details. Store it for future access
           into your 2FA Wallet.
         </div>
-        <div>
-          <div className="new-account-create-form__device-info">
-            <p>Browser</p>
-            <p>{browser}</p>
-          </div>
 
-          <div className="new-account-import-form__select-section">
-            {/* <div className="new-account-import-form__select-label">
-            </div> */}
-            <Select
-              className="new-account-import-form__device-select"
-              name="import-type-select"
-              clearable={false}
-              value={selectedDevice}
-              options={devices}
-              onChange={opt => {
-                console.log(opt)
-                this.setState({ selectedDevice: opt.value });
-              }}
-            />
+        <div className="new-account-create-form__tabs">
+          <div
+            className={`new-account-create-form__tab ${
+              tabValue === 1 ? "new-account-create-form__tab-selected" : ""
+            }`}
+            onClick={() => this.setState({ tabValue: 1 })}
+          >
+            New device
           </div>
+          <div
+            className={`new-account-create-form__tab ${
+              tabValue === 2 ? "new-account-create-form__tab-selected" : ""
+            }`}
+            onClick={() => this.setState({ tabValue: 2 })}
+          >
+            Old device
+          </div>
+        </div>
 
-          <div className="new-account-create-form__buttons">
-            <Button
-              type="default"
-              large
-              className="new-account-create-form__button new-account-create-form__cancel-button"
-              onClick={this.continueWithoutAddingDevice}
-            >
-              Don't add chrome extension
-            </Button>
-            <Button
-              type="secondary"
-              large
-              className="new-account-create-form__button new-account-create-form__confirm-button"
-              onClick={this.addDevice}
-            >
-              Confirm and add
-            </Button>
-          </div>
+        {tabValue === 1 ? this.renderAddNewDevice() : this.renderAddOldDevice()}
+
+        <div className="new-account-create-form__buttons">
+          <Button
+            type="default"
+            large
+            className="new-account-create-form__button new-account-create-form__cancel-button"
+            onClick={this.continueWithoutAddingDevice}
+          >
+            Don't add chrome extension
+          </Button>
+          <Button
+            type="secondary"
+            large
+            className="new-account-create-form__button new-account-create-form__confirm-button"
+            onClick={this.addDevice}
+          >
+            Confirm and add
+          </Button>
         </div>
       </div>
     );
